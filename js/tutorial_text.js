bc38000f6359fb9f06f9b028aea6031316e588a0
Garg-har-06 <gargharshita624@gmail.com>
2022-07-19 20:18:54 +0530

    after image insertion in slides

--- js/tutorial_text.js
+++ js/tutorial_text.js
@@ -1,16 +1,15 @@
 var tutorial_text = [
 	"1)Click on the 'T' button to bring out the tools to draw graph." + "\n" +"\
 	 2)Click on the 'A' button to access the animation tools." +"\n" +"\
-	 3)Click on the button pointed by 'Graph Script' arrow to access the Graph Generation Tool."+"\n" +"\
-	 4)We will learn about adding node in next slide, so click on 'T' and open the tools pannel.",
+	 3)We will learn about adding node in next slide, so click on 'T' and open the tools pannel.",
 	"1)Choose the 'ADD' Option(Or you can press'A' to select add option)"+"\n" +"\
 	 2)Click anywhere on the graph drawing area to add a node."+"\n" +"\
 	 3)The node will be assigned a unique number."+"\n" +"\
 	 4)You can add as many nodes you want,Try adding more Nodes."+"\n" +"\
-	 5)You can't add your own name/number to the node as this feature is not available for now, this will be added at the final release of the application."+"\n" +"\
-	 6)Now we will learn about deleting the Node you have added.",
+	 ",
 	"1)To add a node with CUSTOM NAME you can choose the custom name option in 'NODE ENUM'"+"\n" +"\
-	 2)A prompt will appear after clicking on the board,enter custom name there",
+	 2)A prompt will appear after clicking on the board,enter custom name there"+"\n" +"\
+	 3)Now we will learn about deleting the Node you have added.",
 	"1)Choose the 'DELETE' Option(Or you can press'D' to select delete option)"+"\n" +"\
 	 2)Click on the node you want to delete.",
 	"1)'Whoose!! Node deleted"+"\n" +"\
@@ -30,10 +29,10 @@ var tutorial_text = [
 	   "+" b)Unidirectional Unweighted "+"\n" +"\
 	   "+" c)Bidirectional Weighted "+"\n" +"\
 	   "+" d)Unidirectional Weighted",
-	"1)Foccus on one of the node(ignore this step if already focused)"+"\n" +"\
+	"1)Focus on one of the node(ignore this step if already focused)"+"\n" +"\
 	 2)Now click on another nodes you want to establish connections"+"\n" +"\
 	 3)If the new connection is Unidirectional, then the connection we drawn as outgiong from foccused node to the clicked node"+"\n" +"\
-	 4)To remove the foccus of the current node click on the foccused node itself",
+	 4)To remove the foccus of the current node click on the focused node itself",
 	"Easy till now!!! you can draw large graphs more larger than shown above "+"\n" +"\
 	 Now we will move towards running and visualizing animations on them",
 	"1)Choose the algorithm you want to visualize",
@@ -51,14 +50,6 @@ var tutorial_text = [
 	 3)More delay means slower animations",
 	"1)You can click on the forward and back button to move the frame of slider forward and backward"+"\n" +"\
 	 2)Also you can change the animation frame by using the slider",
-	"(This feature is UNSTABLE)"+"\n" +"\
-	 1)Still you can click on the generate button to generate a graph from the loaded script"+"\n" +"\
-	 2)Each row has a syntax"+"\n" +"\
-	 3)Suppose a row is '1 2 1 98'"+"\n" +"\
-	 4)'1 2' means the connection is to be drawn from 1 to 2"+"\n" +"\
-	 5)'1' after 2 means the type of connection"+"\n" +"\
-	 6)Type '1' means 'bidirectional unweights'"+"\n" +"\
-	 7)And '68' after '1' means the weight of line if line is weighted",
 ]
 
 for (var i = 0;i<slides.length;i++)
